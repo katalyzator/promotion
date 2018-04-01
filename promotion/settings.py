52f70eaed152d2652080afb465a142f6afc6b787
katalyzator <web.coder96@gmail.com>
2018-04-01 13:48:26 +0600

    static configuration added

--- promotion/settings.py
+++ promotion/settings.py
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "promotion", "static", "media")
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "promotion", "static", "static_root")
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(BASE_DIR), "promotion", "templates", "assets", "static_files"),
+)
